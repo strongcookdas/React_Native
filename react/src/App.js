be1e7ae2f62a72a328baab9f527b93931e3f6bec
strongcookdas <goodjav141592@gmail.com>
2023-09-17 16:41:22 +0900

    add : react 복습

--- react/src/App.js
+++ react/src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import "./App.css";
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const [courseGoals, setCourseGoals] = useState([
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other studuents in the Course Q&A" },
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals((prevCourseGoals) => {
+      return prevCourseGoals.concat(newGoal);
+    });
+  };
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
