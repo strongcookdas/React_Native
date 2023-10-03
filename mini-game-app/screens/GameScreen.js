bcebd92872fd136aff09841be0bb71467cae7aa2
strongcookdas <goodjav141592@gmail.com>
2023-10-04 05:21:40 +0900

    add : 64.난수 생성

--- mini-game-app/screens/GameScreen.js
+++ mini-game-app/screens/GameScreen.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen(props) {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      {/* Guess */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
         {/* + - */}
