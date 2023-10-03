0e08ed10d1517ef8717b4478fcfab5c942bcdcf5
strongcookdas <goodjav141592@gmail.com>
2023-10-03 23:26:22 +0900

    add : 프로젝트 초기 설정

---
+++ mini-game-app/screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+function StartGameScreen(props) {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+}
+
+export default StartGameScreen;
