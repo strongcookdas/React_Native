d0d8e727bb114bdff454f0844a1548ec759bd55a
strongcookdas <goodjav141592@gmail.com>
2023-10-03 23:47:01 +0900

    add : 49.그림자 스타일

--- mini-game-app/screens/StartGameScreen.js
+++ mini-game-app/screens/StartGameScreen.js
@@ -1,8 +1,8 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen(props) {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,18 @@ function StartGameScreen(props) {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
