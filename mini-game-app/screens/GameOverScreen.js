6e6bb709c50ebc794e78590592113435f58cee5b
strongcookdas <goodjav141592@gmail.com>
2023-10-06 20:32:21 +0900

    add : 중접 텍스트 사용

--- mini-game-app/screens/GameOverScreen.js
+++ mini-game-app/screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen(props) {
   return (
@@ -12,9 +13,11 @@ function GameOverScreen(props) {
           source={require("../assets/image/success.png")}
         />
       </View>
-      <View>
-        <Text>Your phone needed X rounds to guess the number Y.</Text>
-      </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -40,5 +43,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
