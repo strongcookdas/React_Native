a5cf650205deefcc57aa8f3cbfbd352bbaa61e19
strongcookdas <goodjav141592@gmail.com>
2023-10-06 20:23:19 +0900

    add : 71.배경추가

--- mini-game-app/screens/GameOverScreen.js
+++ mini-game-app/screens/GameOverScreen.js
@@ -1,7 +1,44 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen(props) {
-  return <Text>Game Over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/image/success.png")}
+        />
+      </View>
+      <View>
+        <Text>Your phone needed X rounds to guess the number Y.</Text>
+      </View>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
