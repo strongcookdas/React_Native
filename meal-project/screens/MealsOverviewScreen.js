356a2cd34a38cb2e8031345ed043b4b15c752fd0
strongcookdas <goodjav141592@gmail.com>
2023-10-09 16:10:40 +0900

    add : 헤더 제목 동적 변경

--- meal-project/screens/MealsOverviewScreen.js
+++ meal-project/screens/MealsOverviewScreen.js
@@ -1,15 +1,23 @@
-import { FlatList, StyleSheet } from "react-native";
-import { Text } from "react-native";
-import { View } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import MealItem from "../components/MealItem";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
