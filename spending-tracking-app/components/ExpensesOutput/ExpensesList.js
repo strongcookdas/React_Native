ee7cef8e0e518b4e142288105d0ee5f202412974
strongcookdas <goodjav141592@gmail.com>
2023-10-29 16:43:59 +0900

    feat : print expensesList

--- spending-tracking-app/components/ExpensesOutput/ExpensesList.js
+++ spending-tracking-app/components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,17 @@
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 
-function ExpensesList() {
-  return <FlatList />;
+function renderExpensesItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
+
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpensesItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 }
 
 export default ExpensesList;
